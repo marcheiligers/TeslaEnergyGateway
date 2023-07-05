55e15866870844f44842b13bfba69fe4eccf16e8
Marc Heiligers <marc@eternal.co.za>
2023-07-04 17:03:16 -0700

    Add indexing

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # TeslaEnergyGateway
 
-Monitors my Tesla Energy Gateway from the command line. Makes noise when production stops or starts, and alerts in Slack. Also sends informational messages to Slack every 15 minutes.
+Monitors my Tesla Energy Gateway from the command line. Makes noise when production stops or starts, and alerts in Slack. Also sends informational messages to Slack every 15 minutes and indexes the data into Elasticsearch.
 
 ```bash
 export GW_PWD='password'
 export GW_SLACK='webhookurl'
+export GW_ES_URL='elasticapiurl'
+export GW_ES_API_KEY='elasticapikey'
 ruby stats.rb
 ```
--- stats.rb
+++ stats.rb
@@ -132,6 +132,30 @@ class Slack
   end
 end
 
+class Indexer
+  def initialize(debug: false)
+    @uri = URI.parse(ENV['GW_ES_URL'])
+    @headers = {
+      'Authorization' => "ApiKey #{ENV['GW_ES_API_KEY']}",
+      'Content-Type' => 'application/json'
+    }
+    @http = Net::HTTP.new(@uri.host, @uri.port)
+    @http.use_ssl = true
+    @debug = debug
+  end
+
+  def index(doc)
+    request = Net::HTTP::Post.new('/energy_gateway/_doc/', @headers)
+    request.body = JSON.dump(doc)
+    response = @http.request(request)
+    if @debug
+      puts response.code
+      puts response.body
+    end
+    JSON.parse(response.body)
+  end
+end
+
 class Poller
   POLL_EVERY = 59 # Once a minute with ~1s request time
   RETRY_AFTER = 5
@@ -139,6 +163,7 @@ class Poller
   def initialize
     @gw = Gateway.new
     @slack = Slack.new
+    @indexer = Indexer.new
     @producing = true
   end
 
@@ -156,8 +181,13 @@ class Poller
     `afplay /System/Library/PrivateFrameworks/ScreenReader.framework/Versions/A/Resources/Sounds/#{sound}.aiff &`
   end
 
+  def timestamp
+    # 2020-05-23T18:17:56.715Z
+    Time.now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+  end
+
   def poll
-    @gw.power.merge(@gw.level).merge({ timestamp: Time.now.to_i }).tap do |data|
+    @gw.power.merge(@gw.level).merge({ '@timestamp': timestamp }).tap do |data|
       File.open("data-#{Time.now.strftime('%Y-%m-%d')}.jsonl", 'a') { |f| f.puts JSON.dump(data) }
     end
   rescue => e
@@ -198,9 +228,28 @@ class Poller
       puts "🏠 #{draw(data[:house])}"
       puts "⚡️ #{draw(data[:grid])}"
 
+      @indexer.index(data)
+
       count_down(POLL_EVERY)
     end
+  rescue => e
+    puts "Error: #{e.message}"
+    count_down(RETRY_AFTER)
+    retry
   end
 end
 
 Poller.new.start
+
+# poller = Poller.new
+# doc = {
+#   'grid' => 0.0,
+#   'solar' => 0.0,
+#   'battery' => 0.0,
+#   'house' => 0.0,
+#   'real' => 0.0,
+#   'app' => 0.0,
+#   '@timestamp' => poller.timestamp
+# }
+# indexer = Indexer.new(debug: true)
+# indexer.index(doc)
